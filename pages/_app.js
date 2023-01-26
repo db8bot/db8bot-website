3c98b944ce581be7bfd7d392414599144052f3ba
AirFusion45 <jfang.cv.ca.us@gmail.com>
2023-01-26 11:11:42 -0800

    checking in as a backup before moving to a nextjs theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,46 @@
-import '@/styles/globals.css'
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Routes } from 'react-router-dom';
+import AppRoute from '../utils/AppRoute';
+import ScrollReveal from '../utils/ScrollReveal';
+import ReactGA from 'react-ga';
+import '../assets/scss/style.scss';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Layouts
+import LayoutDefault from '../layouts/LayoutDefault';
+
+// Views 
+import Home from '../views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Routes>
+          <AppRoute exact path="/" component={<Home/} layout={LayoutDefault} />
+        </Routes>
+      )} />
+  );
 }
+
+export default App;
