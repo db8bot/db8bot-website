93fd1b81f14a3e81a29c9f58e4eea5973cbfda18
AirFusion45 <jfang.cv.ca.us@gmail.com>
2023-02-11 20:27:59 -0800

    glow up!

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from 'next/head'
 import image from 'next/image'
 import Link from 'next/link'
 import { HiArrowNarrowRight } from 'react-icons/hi';
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import WidgetBot from '@widgetbot/react-embed'
 
 export default function Home() {
+  const [client, setClient] = useState(false)
+  useEffect(() => setClient(true), [])
   return (
     <div className='py-5 px-[25px] min-h-screen bg-bgBlack flex flex-col'>
-      <div className='h-[100px]' />
+      <div className='md:h-12 lg:h-28 xl:32' />
 
       {/* catch phrase */}
       <div className='flex flex-col items-center'>
@@ -35,11 +37,12 @@ export default function Home() {
       {/* invite btn & learn more btn */}
       <div className='flex flex-col items-center pt-2'>
         <div className='max-w-[600px] flex'>
-          <div className='pr-3'>
-            <button className='rounded-full bg-gradient-to-r from-candy1/75 to-candy2/75 px-5 py-[12px]'>
-              <div className='text-white font-bold flex'>
-                Add To Server <HiArrowNarrowRight className='mt-[1px] ml-2 text-2xl' />
+          <div className='pr-3 group'>
+            <button className='relative parent'>
+              <div className="z-10 whitespace-nowrap rounded-full bg-gradient-to-r from-candy1/75 to-candy2/75 px-5 py-[12px] font-bold text-white">
+                Add To Server &rarr;
               </div>
+              <div className="child top-0 bottom-0 right-0 left-0 absolute inset-0 h-full w-full max-w-0 rounded-full ring-white transition-all duration-1000 group-hover:inline group-hover:max-w-full group-hover:ring-2 z-0"></div>
             </button>
           </div>
 
@@ -54,12 +57,20 @@ export default function Home() {
       </div>
 
       {/* widgetbot */}
-      <div>
-        <WidgetBot
-          server="1072951238905311392"
-          channel="1072951239446368288"
-        />
+      <div className='pt-48 pb-10'>
+
+        <div className="relative group flex justify-center">
+          <div className='absolute w-4/6 h-[700px]'>
+            <div className="absolute inset-0 bg-gradient-to-r from-pink-600 to-purple-600 blur-3xl -inset-3 opacity-80 group-hover:opacity-100 transition duration-1000 group-hover:duration-300"></div>
+          </div>
+          {client && <WidgetBot
+            server='1072951238905311392'
+            channel='1072951239446368288'
+            className='w-4/6 h-[700px] relative'
+          />}
+        </div>
       </div>
+
     </div>
   )
 }
